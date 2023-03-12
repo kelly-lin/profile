1edd6e5e92c7122315c67b5952f60309586a3c7d
Kelly Lin <findkellylin@gmail.com>
2023-03-12 20:00:16 +1100

    add eslint, gh-pages

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -10,6 +10,7 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [],
+  pathPrefix: '/profile'
 }
 
 export default config
